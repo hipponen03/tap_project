f55d1f13228866cbbbcf8a4d4a309c7523596e73
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-02 20:15:41 +0300

    Added and configured framework files

---
+++ app/config/constants.php
@@ -0,0 +1,14 @@
+<?php
+// Project name
+define('PROJECT_NAME', 'kpr');
+// App root dir
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
+
+// db params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'kpr');
+
