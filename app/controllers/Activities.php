82d818be9dafec5b840a34edfadae3da8a3ba34c
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-03 17:36:06 +0300

    Added activity data adding to database fixed

--- app/controllers/Activities.php
+++ app/controllers/Activities.php
@@ -20,4 +20,37 @@ class Activities extends Controller
         );
         $this->view('activities/index', $data);
     }
+
+    public function show($id){
+        $activity = $this->activityModel->getActivity($id);
+        $partners = $this->activityModel->getPartners($id);
+        $data = array(
+            'activity' => $activity,
+            'partners' => $partners
+        );
+        $this->view('activities/show', $data);
+    }
+
+    public function add(){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+            $data = array(
+                'name' => trim($_POST['name']),
+                'name_err' => ''
+            );
+            if(empty($data['name'])){
+                $data['name_err'] = 'Palun lisa teenuse nimetus';
+            }
+            if(empty($data['name_err'])){
+                if($this->activityModel->addActivity($data)){
+                    header('Location: '.URLROOT.'/activities/index');
+                }
+            } else {
+                $this->view('activities/add', $data);
+            }
+        } else {
+            $data = array();
+            $this->view('activities/add', $data);
+        }
+    }
 }
