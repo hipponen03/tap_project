3424bfbb217453e575be2ae2258943e95361bc03
Markkus Hipponen <markkus.hipponen@khk.ee>
2021-08-03 13:05:21 +0300

    Display partner model data at all partners view

--- app/views/partners/index.php
+++ app/views/partners/index.php
@@ -1,8 +1,4 @@
 <?php require_once APPROOT.'/views/inc/header.php'; ?>
-<?php
-echo '<pre>';
-print_r($data);
-?>
 <h1><?php echo $data['title'];?></h1>
 <table class="table table-striped">
     <thead>
@@ -18,46 +14,18 @@ print_r($data);
     </tr>
     </thead>
     <tbody>
+    <?php foreach ($data['partners'] as $partner):?>
     <tr>
-        <th scope="row">1</th>
-        <td>Mark Otto</td>
-        <td>1234</td>
-        <td>mark@otto.com</td>
-        <td>12345678</td>
-        <td>seinad</td>
-        <td>kipsseinad</td>
-        <td>Eesti</td>
+        <th scope="row"><?php echo $partner->id;?></th>
+        <td><?php echo $partner->name;?></td>
+        <td><?php echo $partner->reg_nr;?></td>
+        <td><?php echo $partner->email;?></td>
+        <td><?php echo $partner->phone;?></td>
+        <td><?php echo $partner->activity;?></td>
+        <td><?php echo $partner->activity_details;?></td>
+        <td><?php echo $partner->location;?></td>
     </tr>
-    <tr>
-        <th scope="row">2</th>
-        <td>Mark Otto</td>
-        <td>1234</td>
-        <td>mark@otto.com</td>
-        <td>12345678</td>
-        <td>seinad</td>
-        <td>kipsseinad</td>
-        <td>Eesti</td>
-    </tr>
-    <tr>
-        <th scope="row">3</th>
-        <td>Mark Otto</td>
-        <td>1234</td>
-        <td>mark@otto.com</td>
-        <td>12345678</td>
-        <td>seinad</td>
-        <td>kipsseinad</td>
-        <td>Eesti</td>
-    </tr>
-    <tr>
-        <th scope="row">4</th>
-        <td>Mark Otto</td>
-        <td>1234</td>
-        <td>mark@otto.com</td>
-        <td>12345678</td>
-        <td>seinad</td>
-        <td>kipsseinad</td>
-        <td>Eesti</td>
-    </tr>
+    <?php endforeach;?>
     </tbody>
 </table>
 <?php require_once APPROOT.'/views/inc/footer.php'; ?>
